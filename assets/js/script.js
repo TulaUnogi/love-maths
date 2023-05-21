d3cfcc978d9c97caacfd42cfa7ab26a14a09ae59
TulaUnogi <>
2023-05-21 20:13:21 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,8 +52,10 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert('Hey! You got it right! :D');
+    incrementScore();
   } else {
     alert(`Awww... You answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+    incrementWrongAnswer()
   }
 
   runGame(calculatedAnswer[1]);
@@ -76,12 +78,24 @@ function calculateCorrectAnswer() {
     throw `Unimplemented operator ${operator}. Aborting!`;
   }
 }
+/**
+ * Gets the current score from the DOM and increments it by 1. 
+ */
 
 function incrementScore() {
+  let oldScore = parseInt(document.getElementById('score').innerText);
+  document.getElementById('score').innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1. 
+ */
+
 function incrementWrongAnswer() {
+
+  let oldScore = parseInt(document.getElementById('incorrect').innerText);
+  document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 
